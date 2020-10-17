9c3fb47bc1fb041876b66271fc813b548810ee29
James-Heyes <jheyes@me.com>
2020-10-16 22:26:17 -0400

    Added server

--- twitterBot.py
+++ twitterBot.py
@@ -1,11 +1,21 @@
 import tweepy
 import time
 from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 
-print(ACCESS_KEY)
-#auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-#api = tweepy.API(auth)
-#tweet = "I have slept so long\nI have forgotten the world\nand the world has me."
+def GetTweetString():
+    '''fill with things'''
+    return "string"
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+tweet = "test"
 #api.update_status(tweet)
