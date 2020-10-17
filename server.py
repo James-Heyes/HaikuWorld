e4bdabac26edaaef40f14f5264cfd93ad21a4a9d
James-Heyes <jheyes@me.com>
2020-10-16 22:48:52 -0400

    updated server

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 from os import environ
 from flask import Flask
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
 
 app = Flask(__name__)
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
+
+@app.route('/')
+def hello_world():
+    r = requests.get('http://httpbin.org/status/418')
+    return HttpResponse('<pre>' + r.text + '</pre>')
