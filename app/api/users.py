c3294a3f86a22e92f4c0855a64eda693442b5047
James-Heyes <jheyes@me.com>
2020-11-13 15:06:26 -0500

    Update users.py
    fixed???

--- app/api/users.py
+++ app/api/users.py
@@ -99,16 +99,15 @@ def create_poem():
     if token_auth.current_user().id != 1:
         abort(403)
     data = request.get_json() or {}
-    #if 'body' not in data:
-    #    return bad_request('must include body field')
-    #if Poem.query.filter_by(body=data['body']).first():
-    #    return bad_request('Poem already in database.')
+    if 'body' not in data:
+        return bad_request('must include body field')
+    if Poem.query.filter_by(body=data['body']).first():
+        return bad_request('Poem already in database.')
     poem = Poem()
     poem.from_dict(data)
-    #db.session.add(poem)
-    #db.session.commit()
-    response = jsonify({"fuck":"dicks"})
-    #response = jsonify(poem.to_dict())
+    db.session.add(poem)
+    db.session.commit()
+    response = jsonify(poem.to_dict())
     response.status_code = 201
     #response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
